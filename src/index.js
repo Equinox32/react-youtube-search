5f1a4428ba3eed6c36cc7cc555b4949e38ffd0ae
Equinox32 <tylerangelier@gmail.com>
2018-08-25 14:44:15 -0400

    Added key and installed npm package

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const API_KEY = 'AIzaSyDhC2esTZVGXLuIa-xW4pAZ3xtNfegogJE';
+
+ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
