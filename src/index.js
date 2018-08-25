667d3bf248727cd2d290011c20bb0e8d25687594
Equinox32 <tylerangelier@gmail.com>
2018-08-25 15:08:17 -0400

    AJAX request with React

--- src/index.js
+++ src/index.js
@@ -4,15 +4,35 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDhC2esTZVGXLuIa-xW4pAZ3xtNfegogJE';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<SearchBar />
+// 		</div>
+// 	);
+// };
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'bmw' }, videos => {
+			// ES6 syntax for this.setState({ videos: videos});
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
