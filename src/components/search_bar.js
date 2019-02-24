68db828c5293b630de82af51f86a0bef19dc94f7
Equinox32 <tylerangelier@gmail.com>
2019-02-24 13:37:25 -0500

    updated search bar with react-bootstrap

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 
 class SearchBar extends React.Component {
 	constructor(props) {
@@ -18,11 +20,22 @@ class SearchBar extends React.Component {
 	}
 	render() {
 		return (
-			<div className="search-bar">
-				<input
-					value={this.state.term}
-					onChange={event => this.onInputChange(event.target.value)}
-				/>{' '}
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+						<Form className="form-horizontal">
+							<FormControl
+								type="text"
+								placeholder="Search"
+								className="form-control"
+								value={this.state.term}
+								onChange={event =>
+									this.onInputChange(event.target.value)
+								}
+							/>
+						</Form>
+					</div>
+				</div>
 			</div>
 		);
 	}
